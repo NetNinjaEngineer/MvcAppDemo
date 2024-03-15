e4a83e736ad06cdd31a7e49603db12d0734c360d
NetNinjaEngineer <me5260287@outlook.com>
2024-03-15 12:15:21 +0200

    Implemented Login Functionality

--- Demo.PL/Controllers/AccountController.cs
+++ Demo.PL/Controllers/AccountController.cs
@@ -6,45 +6,78 @@ using System.Threading.Tasks;
 
 namespace Demo.PL.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly UserManager<ApplicationUser> _userManager;
+	public class AccountController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
 
-        public IActionResult Register()
-        {
-            return View();
-        }
+		public IActionResult Register()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var appUser = new ApplicationUser
-                {
-                    FirstName = model.FName,
-                    LastName = model.LName,
-                    UserName = model.Email.Split('@')[0],
-                    IsAgree = model.IsAgree,
-                    Email = model.Email
-                };
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var appUser = new ApplicationUser
+				{
+					FirstName = model.FName,
+					LastName = model.LName,
+					UserName = model.Email.Split('@')[0],
+					IsAgree = model.IsAgree,
+					Email = model.Email
+				};
 
-                var result = await _userManager.CreateAsync(appUser, model.Password);
+				var result = await _userManager.CreateAsync(appUser, model.Password);
 
-                if (result.Succeeded)
-                    return RedirectToAction("Login");
+				if (result.Succeeded)
+					return RedirectToAction(nameof(Login));
 
-                else
-                    foreach (var error in result.Errors)
-                        ModelState.AddModelError(string.Empty, error.Description);
-            }
+				else
+					foreach (var error in result.Errors)
+						ModelState.AddModelError(string.Empty, error.Description);
+			}
 
-            return View(model);
-        }
-    }
+			return View(model);
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				ApplicationUser appUser = await _userManager.FindByEmailAsync(model.Email);
+				if (appUser is not null)
+				{
+					bool isPasswordCorrect = await _userManager.CheckPasswordAsync(appUser, model.Password);
+					if (isPasswordCorrect)
+					{
+						// Login
+						var loginResult = await _signInManager.PasswordSignInAsync(appUser, model.Password, model.RememberMe, false);
+						if (loginResult.Succeeded)
+							return RedirectToAction("Index", "Home");
+					}
+				}
+				else
+					ModelState.AddModelError(string.Empty, "Invalid Credentials.");
+
+			}
+
+			return View(model);
+
+		}
+	}
 }
