bfa04da7224acd27de3f418a6781f874398959bc
NetNinjaEngineer <me5260287@outlook.com>
2024-03-16 12:54:36 +0200

    Implemented Get all Roles and Create Role Index model

---
+++ Demo.PL/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Demo.PL.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Demo.PL.Controllers
+{
+    [Authorize]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, IMapper mapper)
+        {
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string searchValue)
+        {
+            if (string.IsNullOrEmpty(searchValue))
+            {
+                List<IdentityRole> roles = await _roleManager.Roles.ToListAsync();
+                IEnumerable<RoleViewModel> mappedRoles = _mapper.Map<IEnumerable<IdentityRole>, IEnumerable<RoleViewModel>>(roles);
+                return View(mappedRoles);
+            }
+            else
+            {
+                var role = await _roleManager.FindByNameAsync(searchValue);
+                if (role is null)
+                {
+                    return View();
+                }
+
+                var mappedRole = _mapper.Map<IdentityRole, RoleViewModel>(role);
+                return View(new List<RoleViewModel> { mappedRole });
+            }
+        }
+    }
+}
---
+++ Demo.PL/Profiles/RoleProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Demo.PL.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace Demo.PL.Profiles
+{
+    public class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            CreateMap<IdentityRole, RoleViewModel>()
+                .ForMember(x => x.RoleName, opt => opt.MapFrom(x => x.Name))
+                .ForMember(x => x.RoleId, opt => opt.MapFrom(x => x.Id))
+                .ReverseMap();
+        }
+    }
+}
